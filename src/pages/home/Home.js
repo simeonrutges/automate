d4e711cf943ffab46ab838c267b040fe3f5722db
SRutges <104904365+628HG09@users.noreply.github.com>
2023-03-15 21:39:31 +0100

    de blokken moeten nog gepositioneerd worden

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -10,20 +10,22 @@ function Home() {
         <home className="outer-content-container">
             <div className="inner-content-container">
 
-                {/*Waarom kan ik deze div niet zomaar in section veranderen? Er veranderd iets in CSS*/}
-                    <div className="home">
+                {/*Waarom heeft de margin 50px? -> App.css */}
+                <section className="home">
                         <img src={picture} alt="beschrijving van de afbeelding" />
                         <div className="textvlak">
                             <h1>De slimmer keuze voor milieubewuste reizigers</h1>
-                            <p>Beschrijving van de tekst</p>
                         </div>
-                    </div>
+                        <div className="blok">
+                            <h1>hier komt het blok</h1>
+                        </div>
+                </section>
 
-            <section>
-                <p>Als je ingelogd bent, bekijk dan de <Link to="/profile">Profielpagina</Link></p>
+                <section>
+                   <p>Als je ingelogd bent, bekijk dan de <Link to="/profile">Profielpagina</Link></p>
                 <p>Je kunt ook <Link to="/signin">inloggen</Link> of jezelf <Link to="/signup">registeren</Link> als je nog geen
                     account hebt.</p>
-            </section>
+                </section>
 
             </div>
         </home>
