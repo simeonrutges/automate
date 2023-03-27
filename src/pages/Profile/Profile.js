a8790f93aa4569622364585e4244c9fcf762167b
SRutges <104904365+628HG09@users.noreply.github.com>
2023-03-27 22:05:35 +0200

    opweg profile page

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,12 +1,13 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { AuthContext } from '../../context/AuthContext';
+import React, {useContext, useEffect, useState} from 'react';
+import {Link} from 'react-router-dom';
+import {AuthContext} from '../../context/AuthContext';
 import axios from 'axios';
+import  './profile.css';
 
 function Profile() {
     const [profileData, setProfileData] = useState({});
 
-    const { user } = useContext(AuthContext);
+    const {user} = useContext(AuthContext);
 
     // useEffect(() => {
     //     // we halen de pagina-content op in de mounting-cycle
@@ -32,7 +33,7 @@ function Profile() {
     // }, [])
 
     // Deze heb ik bijgevoegd om te proberen/bovenstaand was goed!:
-  const username = user.username;
+    const username = user.username;
 
     useEffect(() => {
         async function fetchProfileData() {
@@ -63,41 +64,107 @@ function Profile() {
 
         fetchProfileData();
     }, []);
+    //////////////////////////
+    const [bio, setBio] = useState('');
+    const [vehicle, setVehicle] = useState('');
+    const [licensePlate, setLicensePlate] = useState('Kenteken');
+    const [model, setModel] = useState('Model');
+    const [brand, setBrand] = useState('Merk')
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(`Bio: ${bio}`);
+        console.log(`Voertuig: ${vehicle}`);
+        // voeg hier de code toe om de biografie en het voertuig op te slaan
+    }
+
 
 
     return (
         <profile className="outer-content-container">
             <div className="inner-content-container">
 
-        <div>
-            <h1>Mijn Profiel</h1>
-            <div>
-            <section>
-                <h2>FOTO</h2>
-                <h2>username: {user.username}</h2>
-            </section>
-            </div>
+                <div>
+                    <h1>Mijn Profiel</h1>
 
 
-            {/*Als er keys in ons object zitten hebben we data, en dan renderen we de content*/}
-            {Object.keys(profileData).length > 0 &&
-                <section>
-                    <h2>Strikt geheime profiel-content</h2>
-                    <h3>{profileData.email}</h3>
-                    <p>{profileData.roles}</p>
-                    <p>{profileData.firstname}</p>
-                    <p>{profileData.lastname}</p>
+                    <div>
+                        <section className="foto-name">
+                            <h4>FOTO</h4>
+                            <h4>{user.username}</h4>
+                        </section>
+                    </div>
+
+                    <div>
+                        <form onSubmit={handleSubmit}>
+                            <label htmlFor="bio-field">
+                                Biografie toevoegen:
+                                <textarea
+                                    id="bio-field"
+                                    name="bio"
+                                    value={bio}
+                                    onChange={(e) => setBio(e.target.value)}
+                                />
+                            </label>
+
+                            <label htmlFor="vehicle-field">
+                                Voertuig toevoegen
+                                <input
+                                    type="text"
+                                    id="vehicle-field"
+                                    name="vehicle"
+                                    value={vehicle}
+                                    onChange={(e) => setVehicle(e.target.value)}
+                                />
+                            </label>
+                            <label htmlFor="licensePlate-field">Kenteken:</label>
+                            <input
+                                type="text"
+                                id="licensePlate-field"
+                                value={licensePlate}
+                                onChange={(e) => setLicensePlate(e.target.value)}
+                            />
+
+                            <label htmlFor="model-field">Model:</label>
+                            <input
+                                type="text"
+                                id="model-field"
+                                value={model}
+                                onChange={(e) => setModel(e.target.value)}
+                            />
+
+                            <label htmlFor="brand-field">Merk:</label>
+                            <input
+                                type="text"
+                                id="brand-field"
+                                value={brand}
+                                onChange={(e) => setBrand(e.target.value)}
+                            />
+
+                            <button
+                                type="submit"
+                                className="form-button"
+                            >
+                                Opslaan
+                            </button>
+                        </form>
+                    </div>
 
 
+                    {/*Als er keys in ons object zitten hebben we data, en dan renderen we de content*/}
+                    {Object.keys(profileData).length > 0 &&
+                        <section>
+                            <h2>Strikt geheime profiel-content</h2>
+                            <h3>{profileData.email}</h3>
+                            <p>{profileData.roles}</p>
+                            <p>{profileData.firstname}</p>
+                            <p>{profileData.lastname}</p>
 
 
-                </section>
-            }
-            <p>Terug naar de <Link to="/">Homepagina</Link></p>
-        </div>
-
-
-
+                        </section>
+                    }
+                    <p>Terug naar de <Link to="/">Homepagina</Link></p>
+                </div>
 
 
             </div>
