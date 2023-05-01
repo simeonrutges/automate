4bec634fb2ced43e84d30fe16c68f2021400976e
SRutges <104904365+628HG09@users.noreply.github.com>
2023-05-01 18:25:27 +0200

    ritten toegevoegd op scherm

--- src/pages/MyRides/MyRides.js
+++ src/pages/MyRides/MyRides.js
@@ -1,15 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import axios from "axios";
+import {AuthContext} from "../../context/AuthContext";
 
 function MyRides() {
+    const {isAuth, logout, isBestuurder, isPassagier, user} = useContext(AuthContext);
+
     const [rides, setRides] = useState([]);
-    // const id = 'yourUserId'; // Replace this with the actual user ID you want to use
+    // const username = 'yourUserId'; // Replace this with the actual user ID you want to use
 
     useEffect(() => {
     async function fetchRides() {
         try {
-            const response = await axios.get('https://localhost:8080/rides/');
+            const username = isAuth && user.username ? user.username : '';
+            console.log("username: " + username);
+
+            const response = await axios.get(`http://localhost:8080/users/${username}/rides`);
+
             setRides(response.data);
+            console.log("data...:"  + response.data);
         } catch (error) {
             console.error(error);
         }
@@ -24,11 +32,11 @@ function MyRides() {
                 {rides.map((ride) => (
                     <div key={ride.id} className="ride">
                         {/* Display relevant ride information */}
-                        <h3>Rit: {ride.name}</h3>
-                        <p>Vertrek: {ride.departure}</p>
-                        <p>Aankomst: {ride.arrival}</p>
-                        <p>Datum: {ride.date}</p>
-                        <p>Tijd: {ride.time}</p>
+                        <h3>Destination: {ride.destination}</h3>
+                        <p>Vertrek: {ride.pickUpLocation}</p>
+                        {/*<p>Aankomst: {ride.arrival}</p>*/}
+                        {/*<p>Datum: {ride.date}</p>*/}
+                        {/*<p>Tijd: {ride.time}</p>*/}
                     </div>
                 ))}
             </div>
