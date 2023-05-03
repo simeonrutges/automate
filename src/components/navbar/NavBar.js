425520a5247a93957872e84f9aca31171a03b4b5
SRutges <104904365+628HG09@users.noreply.github.com>
2023-05-03 16:37:32 +0200

    berichten gestart. Fetchdata werkt+ styling

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -7,137 +7,155 @@ import './navbar.css';
 function NavBar() {
     const { isAuth, logout } = useContext(AuthContext);
     const history = useHistory();
-
-    // test
     const location = useLocation();
+    const isHomePage = location.pathname === '/';
     const isProfilePage = location.pathname === '/profile';
-// einde
+    const isMessagesPage = location.pathname === '/my-messages';
+
+
+//     return (
+//         <navbar className="outer-content-container">
+//             <div className="inner-content-container">
+//                 <nav className="nav">
+//
+//             <Link to="/">
+//           <span className="logo-container">
+//             <img src={logo} alt="logo"/>
+//             <h3>
+//               AutoMate
+//             </h3>
+//           </span>
+//             </Link>
+//
+//                     <span className="nav-buttons">
+//             {!isProfilePage && (
+//                 <button
+//                     type="button"
+//                     onClick={() => history.push('/?section=how-it-works')}
+//                 >
+//                     Hoe werkt het?
+//                 </button>
+//             )}
+//
+//                         {isAuth ? (
+//                             <div>
+//                                 {isProfilePage ? (
+//                                     <>
+//                                         <button
+//                                             type="button"
+//                                             onClick={() => history.push('/my-rides')}
+//                                         >
+//                                             Mijn ritten
+//                                         </button>
+//                                         <button
+//                                             type="button"
+//                                             onClick={() => history.push('/my-messages')}
+//                                         >
+//                                             Mijn berichten
+//                                         </button>
+//                                     </>
+//                                 ) : (
+//                                     <button
+//                                         type="button"
+//                                         onClick={() => history.push('/profile')}
+//                                     >
+//                                         Profielpagina
+//                                     </button>
+//                                 )}
+//                                 <button
+//                                     type="button"
+//                                     onClick={logout}
+//                                 >
+//                                     Log uit
+//                                 </button>
+//                             </div>
+//                         ) : (
+//                             !isProfilePage && (
+//                                 <div>
+//                                     <button
+//                                         type="button"
+//                                         onClick={() => history.push('/signin')}
+//                                     >
+//                                         Inloggen
+//                                     </button>
+//                                     <button
+//                                         type="button"
+//                                         onClick={() => history.push('/signup')}
+//                                     >
+//                                         Aanmelden
+//                                     </button>
+//                                 </div>
+//                             )
+//                         )}
+//             </span>
+//                 </nav>
+//             </div>
+//         </navbar>
+//     );
+// }
+//
+// export default NavBar;
+
+    const navButton = (path, label) => (
+        <button type="button" onClick={() => history.push(path)}>
+            {label}
+        </button>
+    );
 
     return (
         <navbar className="outer-content-container">
             <div className="inner-content-container">
                 <nav className="nav">
-
-            <Link to="/">
-          <span className="logo-container">
-            <img src={logo} alt="logo"/>
-            <h3>
-              AutoMate
-            </h3>
-          </span>
-            </Link>
-
-{/*                    <span className="nav-buttons">*/}
-{/*            <button*/}
-{/*                type="button"*/}
-{/*                onClick={() => history.push('/?section=how-it-works')}*/}
-{/*            >*/}
-{/*                Hoe werkt het?*/}
-{/*            </button>*/}
-
-{/*            {isAuth ?*/}
-{/*                <div>*/}
-{/*                    <button*/}
-{/*                        type="button"*/}
-{/*                        onClick={() => history.push('/profile')}*/}
-{/*                    >*/}
-{/*                        Profielpagina*/}
-{/*                    </button>*/}
-{/*                <button*/}
-{/*                    type="button"*/}
-{/*                    onClick={logout}*/}
-{/*                >*/}
-{/*                    Log uit*/}
-{/*                </button>*/}
-{/*                </div>*/}
-{/*                :*/}
-{/*                <div>*/}
-{/*                    <button*/}
-{/*                        type="button"*/}
-{/*                        onClick={() => history.push('/signin')}*/}
-{/*                    >*/}
-{/*                        Inloggen*/}
-{/*                    </button>*/}
-{/*                    <button*/}
-{/*                        type="button"*/}
-{/*                        onClick={() => history.push('/signup')}*/}
-{/*                    >*/}
-{/*                        Aanmelden*/}
-{/*                    </button>*/}
-{/*                </div>*/}
-{/*            }*/}
-{/*            </span>*/}
-
-
-{/*        </nav>*/}
-{/*            </div>*/}
-{/*        </navbar>*/}
-{/*    );*/}
-{/*}*/}
-
-{/*export default NavBar;*/}
+                    <Link to="/">
+            <span className="logo-container">
+              <img src={logo} alt="logo" />
+              <h3 className="brand">AutoMate</h3>
+            </span>
+                    </Link>
 
                     <span className="nav-buttons">
-            {!isProfilePage && (
-                <button
-                    type="button"
-                    onClick={() => history.push('/?section=how-it-works')}
-                >
-                    Hoe werkt het?
-                </button>
-            )}
-
+            {!isHomePage && navButton('/', 'Homepagina')}
+                        {isHomePage && (
+                            <>
+                                {isAuth ? (
+                                    navButton('/?section=how-it-works', 'Hoe werkt het?')
+                                ) : (
+                                    <>
+                                        {navButton('/signin', 'Inloggen')}
+                                        {navButton('/signup', 'Aanmelden')}
+                                        {navButton('/?section=how-it-works', 'Hoe werkt het?')}
+                                    </>
+                                )}
+                            </>
+                        )}
                         {isAuth ? (
-                            <div>
+                            <>
                                 {isProfilePage ? (
                                     <>
-                                        <button
-                                            type="button"
-                                            onClick={() => history.push('/my-rides')}
-                                        >
-                                            Mijn ritten
-                                        </button>
-                                        <button
-                                            type="button"
-                                            onClick={() => history.push('/my-messages')}
-                                        >
-                                            Mijn berichten
-                                        </button>
+                                        {navButton('/my-rides', 'Mijn ritten')}
+                                        {navButton('/my-messages', 'Mijn berichten')}
+                                    </>
+                                ) : isMessagesPage ? (
+                                    <>
+                                        {navButton('/my-rides', 'Mijn ritten')}
+                                        {navButton('/profile', 'Profielpagina')}
                                     </>
                                 ) : (
-                                    <button
-                                        type="button"
-                                        onClick={() => history.push('/profile')}
-                                    >
-                                        Profielpagina
-                                    </button>
+                                    navButton('/profile', 'Profielpagina')
                                 )}
-                                <button
-                                    type="button"
-                                    onClick={logout}
-                                >
+                                <button type="button" onClick={logout}>
                                     Log uit
                                 </button>
-                            </div>
+                            </>
                         ) : (
-                            !isProfilePage && (
-                                <div>
-                                    <button
-                                        type="button"
-                                        onClick={() => history.push('/signin')}
-                                    >
-                                        Inloggen
-                                    </button>
-                                    <button
-                                        type="button"
-                                        onClick={() => history.push('/signup')}
-                                    >
-                                        Aanmelden
-                                    </button>
-                                </div>
+                            !isHomePage && (
+                                <>
+                                    {navButton('/signin', 'Inloggen')}
+                                    {navButton('/signup', 'Aanmelden')}
+                                </>
                             )
                         )}
-            </span>
+          </span>
                 </nav>
             </div>
         </navbar>
--- src/pages/MyMessages/MyMessages.js
+++ src/pages/MyMessages/MyMessages.js
@@ -1,6 +1,7 @@
 import React, {useContext, useEffect, useState} from 'react';
 import axios from "axios";
 import {AuthContext} from "../../context/AuthContext";
+import {Link} from "react-router-dom";
 
 function MyMessages() {
     const {isAuth, logout, isBestuurder, isPassagier, user} = useContext(AuthContext);
@@ -16,9 +17,10 @@ function MyMessages() {
                 console.log("username: " + username); // is wel nodig volgens mij
 
                 // Voeg eventueel authenticatie headers toe als dat nodig is
-                const response = await axios.get('http://localhost:8080/notifications');
-               setMessages(response.data);
-                console.log(messages);
+                const response = await axios.get(`http://localhost:8080/notifications/user/${username}`);
+                console.log("response.data fetchMessages: ", response.data);
+                setMessages(response.data);
+
 
             } catch (e) {
                 setError(e.message);
@@ -35,9 +37,15 @@ function MyMessages() {
                 <p>Er is een fout opgetreden bij het ophalen van berichten: {error}</p>
             ) : (
                 <ul>
+                    {/*{messages.map((message) => (*/}
+                    {/*    <li key={message.id}>{message.text}</li>*/}
+                    {/*))}*/}
                     {messages.map((message) => (
-                        <li key={message.id}>{message.text}</li>
+                        <li key={message.id}>
+                            {message.type} {message.sentDate}. Voor meer informatie ga naar <Link to="/my-rides">"Mijn ritten"</Link>. afzender: {message.sender.username}
+                        </li>
                     ))}
+
                 </ul>
             )}
         </div>
