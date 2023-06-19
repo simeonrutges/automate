dc68ec5c823d09fbc7870d0a66d95e730b6bdcd9
SRutges <104904365+628HG09@users.noreply.github.com>
2023-06-19 14:30:12 +0200

    sign up layout compl

--- src/pages/signUp/SignUp.js
+++ src/pages/signUp/SignUp.js
@@ -130,28 +130,59 @@ function SignUpTest() {
                         onChange={handlePhoneNumberChange}
                     />
 
-                    <label htmlFor="driver-radio" className="radio">
-                        <input
-                            type="radio"
-                            id="driver-radio"
-                            value="BESTUURDER"
-                            name="role"
-                            checked={role === "BESTUURDER"}
-                            onChange={handleRoleChange}
-                        />
-                        Bestuurder
-                    </label>
-                    <label htmlFor="passenger-radio" className="radio">
-                        <input
-                            type="radio"
-                            id="passenger-radio"
-                            value="PASSAGIER"
-                            name="role"
-                            checked={role === "PASSAGIER"}
-                            onChange={handleRoleChange}
-                        />
-                        Passagier
-                    </label>
+                    {/*<label htmlFor="driver-radio" className="radio">*/}
+                    {/*    <input*/}
+                    {/*        type="radio"*/}
+                    {/*        id="driver-radio"*/}
+                    {/*        value="BESTUURDER"*/}
+                    {/*        name="role"*/}
+                    {/*        checked={role === "BESTUURDER"}*/}
+                    {/*        onChange={handleRoleChange}*/}
+                    {/*    />*/}
+                    {/*    Bestuurder*/}
+                    {/*</label>*/}
+                    {/*<label htmlFor="passenger-radio" className="radio">*/}
+                    {/*    <input*/}
+                    {/*        type="radio"*/}
+                    {/*        id="passenger-radio"*/}
+                    {/*        value="PASSAGIER"*/}
+                    {/*        name="role"*/}
+                    {/*        checked={role === "PASSAGIER"}*/}
+                    {/*        onChange={handleRoleChange}*/}
+                    {/*    />*/}
+                    {/*    Passagier*/}
+                    {/*</label>*/}
+ <section id="radio-section">
+                    <div className="radio">
+                        <label htmlFor="driver-radio">
+                            Bestuurder
+                            <input
+                                type="radio"
+                                id="driver-radio"
+                                value="BESTUURDER"
+                                name="role"
+                                checked={role === "BESTUURDER"}
+                                onChange={handleRoleChange}
+                            />
+                        </label>
+                    </div>
+                    <div className="radio passenger-radio">
+                        <label htmlFor="passenger-radio">
+                            Passagier
+                            <input
+                                type="radio"
+                                id="passenger-radio"
+                                value="PASSAGIER"
+                                name="role"
+                                checked={role === "PASSAGIER"}
+                                onChange={handleRoleChange}
+                            />
+                        </label>
+                    </div>
+ </section>
+
+
+
 
                     <button type="submit">Sign Up</button>
                 </form>
